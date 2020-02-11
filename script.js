1338425c3094fada26fdaa34e3bcc195d32f89a1
Joke Heyndels <jo_hannah@outlook.com>
2020-02-11 19:15:16 +0100

    Add date function (innerHTML)

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const secondHand = document.querySelector(".second-hand")
 const minuteHand = document.querySelector(".minute-hand")
 const hourHand = document.querySelector(".hour-hand")
+const dayBox = document.querySelector(".day")
+const monthBox = document.querySelector(".month")
+const yearBox = document.querySelector(".year")
+const weekdayBox = document.querySelector(".weekday")
 
 function setDate() {
     const now = new Date();
@@ -13,6 +17,13 @@ function setDate() {
     const hours = now.getHours();
     const hoursDegrees = (hours / 12) * 360 + ( minutesDegrees - 90 ) / 12 + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    weekdayNum = now.getDay();
+    week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    weekdayBox.innerHTML = week[weekdayNum];
+    dayBox.innerHTML = now.getDate();
+    monthBox.innerHTML = now.getMonth() + 1;
+    yearBox.innerHTML = now.getFullYear()
+
 }
 
 
