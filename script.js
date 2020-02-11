a9607325e80c35dc5e09b6872b71331a87742a60
Joke Heyndels <jo_hannah@outlook.com>
2020-02-11 09:25:56 +0100

    Add hours and minutes hand - end of tutorial

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const secondHand = document.querySelector(".second-hand")
+const minuteHand = document.querySelector(".minute-hand")
+const hourHand = document.querySelector(".hour-hand")
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
+    const secondsDegrees = seconds * 6 + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     const minutes = now.getMinutes();
-    const secondsDegrees = seconds * 6 + 90
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-    console.log(secondsDegrees);
+    const minutesDegrees = minutes * 6 + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    const hours = now.getHours();
+    const hoursDegrees = (hours/12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 
